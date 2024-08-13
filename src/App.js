85c616995f1cc2c64dee2bc1ad6cee064a889c9a
david gomes <davidgomesk8@gmail.com>
2024-08-13 15:03:20 +0200

    tic-tact-toe app done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Square from "./components/Square";
+import { Patterns } from "./Patterns";
 
 function App() {
-  const [board, setBoard] = useState(("", "", "", "", "", "", "", "", ""));
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState("X");
+  const [result, setResult] = useState({ winner: "none", state: "none" });
+
+  useEffect(() => {
+    checkWin();
+    checkIfTie();
+
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state != "none") {
+      alert(`Game finished! Winning player: ${result.winner}`);
+      restartGame();
+    }
+  }, [result]);
 
   const chooseSquare = (square) => {
     setBoard(
@@ -16,11 +36,39 @@ function App() {
         return val;
       })
     );
-    if (player == "X") {
-      setPlayer("O");
-    } else {
-      setPlayer("X");
+  };
+
+  const checkWin = () => {
+    Patterns.forEach((currPattern) => {
+      const firstPlayer = board[currPattern[0]];
+      if (firstPlayer == "") return;
+      let foundWinningPattern = true;
+      currPattern.forEach((idx) => {
+        if (board[idx] != firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "Won" });
+      }
+    });
+  };
+
+  const checkIfTie = () => {
+    let filled = true;
+    board.forEach((square) => {
+      if (square == "") {
+        filled = false;
+      }
+    });
+    if (filled) {
+      setResult({ winner: "No one", state: "Tie" });
     }
+  };
+
+  const restartGame = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setPlayer("O");
   };
 
   return (
@@ -46,8 +94,46 @@ function App() {
             }}
           />
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Square
+            val={board[3]}
+            chooseSquare={() => {
+              chooseSquare(3);
+            }}
+          />
+          <Square
+            val={board[4]}
+            chooseSquare={() => {
+              chooseSquare(4);
+            }}
+          />
+          <Square
+            val={board[5]}
+            chooseSquare={() => {
+              chooseSquare(5);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[6]}
+            chooseSquare={() => {
+              chooseSquare(6);
+            }}
+          />
+          <Square
+            val={board[7]}
+            chooseSquare={() => {
+              chooseSquare(7);
+            }}
+          />
+          <Square
+            val={board[8]}
+            chooseSquare={() => {
+              chooseSquare(8);
+            }}
+          />
+        </div>
       </div>
     </div>
   );
